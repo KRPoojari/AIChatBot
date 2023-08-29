3cabc4e87ca6678236213d63d0c1d81991829fec
sarafatima5 <fatimasara5977@gmail.com>
2023-08-29 14:48:47 +0530

    responsemodel

---
+++ Backend/src/main/java/com/rest/responseModel/QuestionnaireCompanyResponse.java
@@ -0,0 +1,22 @@
+package com.rest.responseModel;
+
+public class QuestionnaireCompanyResponse {
+
+    private String answer;
+    private String description;
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Backend/src/main/java/com/rest/responseModel/ResponseEntity.java
@@ -0,0 +1,13 @@
+package com.rest.responseModel;
+
+public class ResponseEntity {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
