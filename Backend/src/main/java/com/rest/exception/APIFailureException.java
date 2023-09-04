42bc0f207175031b849c7af8aef36369e1bece42
Thanushree <thanushreeyadav2001@gmail.com>
2023-09-04 16:57:14 +0530

    exceptions added

---
+++ Backend/src/main/java/com/rest/exception/APIFailureException.java
@@ -0,0 +1,8 @@
+package com.rest.exception;
+
+public class APIFailureException  extends Exception{
+
+    public APIFailureException(String message){
+        super(message);
+    }
+}
---
+++ Backend/src/main/java/com/rest/exception/IncompleteDetailsProvided.java
@@ -0,0 +1,5 @@
+package com.rest.exception;
+
+public class IncompleteDetailsProvided extends Exception{
+    public IncompleteDetailsProvided(String message){super(message);}
+}
