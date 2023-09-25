129c6d90baca43bf7864d6c78dc049cf4fdf55ce
Aishwarya S <83657227+EXO-BYTE@users.noreply.github.com>
2023-09-25 11:29:20 +0530

    Onboarding Test Cases

--- Chatbot-frontendfinal/Chatbot-frontendfinal/src/Components/Chatbot/OnboardingProgress.test.js
+++ Chatbot-frontendfinal/Chatbot-frontendfinal/src/Components/Chatbot/OnboardingProgress.test.js
@@ -1,40 +1,29 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import OnboardingProgress from './OnboardingProgress';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders the component with initial state', () => {
-  render(<OnboardingProgress />);
-  
-  // Check if the initial progress is 0%
-  expect(screen.getByText('0% complete')).toBeInTheDocument();
+describe('OnboardingProgress Component', () => {
+  it('renders the component', () => {
+    render(<OnboardingProgress />);
+    const heading = screen.getByText('Onboarding Progress');
+    expect(heading).toBeInTheDocument();
+  });
 
-  // Check if all questions are present
-  expect(screen.getByText('Have you attended the induction program?')).toBeInTheDocument();
-  expect(screen.getByText('Did you apply for the ID card?')).toBeInTheDocument();
-  expect(screen.getByText('Have you received Telstra Goodies?')).toBeInTheDocument();
-  expect(screen.getByText('Did the chapter heads connect with you?')).toBeInTheDocument();
+  it('initial progress is 0%', () => {
+    render(<OnboardingProgress />);
+    const progressText = screen.getByText('0% complete');
+    expect(progressText).toBeInTheDocument();
+  });
 
-  // Check if placeholder text is present in input fields
-  const inputFields = screen.getAllByPlaceholderText('Yes or No');
-  expect(inputFields).toHaveLength(4);
-});
+  test('updates progress when "Yes" is selected for the first question', () => {
+    render(<OnboardingProgress />);
+    
+    const progressText = screen.getByText(/0% complete/i);
+    expect(progressText).toBeInTheDocument();
+    
+    fireEvent.click(screen.getByLabelText('Yes', { selector: 'input[type="radio"][name="question-1"]' }));
 
-test('updates progress when "Yes" is selected', () => {
-  render(<OnboardingProgress />);
-
-  // Set the "Yes" option for the first question
-  fireEvent.click(screen.getByLabelText('Yes', { selector: 'input[type="radio"]' }));
-
-  // Check if the progress has been updated to 25%
-  expect(screen.getByText('25% complete')).toBeInTheDocument();
-});
-
-test('does not update progress when "No" is selected', () => {
-  render(<OnboardingProgress />);
-
-  // Set the "No" option for the first question
-  fireEvent.click(screen.getByLabelText('No', { selector: 'input[type="radio"]' }));
-
-  // Check if the progress remains 0%
-  expect(screen.getByText('0% complete')).toBeInTheDocument();
-});
+    expect(progressText).toHaveTextContent(/25% complete/i);
+  });
+  });
